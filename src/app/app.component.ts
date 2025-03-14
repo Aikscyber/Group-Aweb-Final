38d0344b197ea6414e976d7606648e8194a40228
pooxhi <rak.mariano6262@gmail.com>
2025-03-14 17:36:15 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { AboutComponent } from './about/about.component';
 import { LostAndFoundComponent } from './lost-and-found/lost-and-found.component';
 import { AdaptionComponent } from './adaption/adaption.component';
 import { ContactComponent } from './contact/contact.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutComponent, LostAndFoundComponent, AboutComponent, AdaptionComponent, ContactComponent],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
